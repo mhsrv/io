d8a5334c429bcab505da0cf08326b69a41b79338
Mihail Mojsoski <mihail@mojsoski.mk>
2022-09-07 16:00:06 +0200

    Added read and write to io.h

--- async/io.h
+++ async/io.h
@@ -8,4 +8,14 @@ namespace io {
             io_uring_prep_mkdir(sqe, path.c_str(), mode);
         });
     }
+    uint32_t write(int fd, const char *buf, uint32_t amount, size_t offset = 0) {
+        return event_loop::self().request([fd, buf, amount, offset](io_uring_sqe* sqe) {
+            io_uring_prep_write(sqe, fd, buf, amount, offset);
+        });
+    }
+    uint32_t read(int fd, char *buf, uint32_t amount, size_t offset = 0) {
+        return event_loop::self().request([fd, buf, amount, offset](io_uring_sqe* sqe) {
+            io_uring_prep_read(sqe, fd, buf, amount, offset);
+        });
+    }
 }
