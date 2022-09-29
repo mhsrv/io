a65efdffe17f4e78e081e192284c5a40cb8f1089
Mihail Mojsoski <mihail@mojsoski.mk>
2022-09-29 17:09:47 +0200

    In io: + added most (if not all) of the async operations.

--- async/lib/io.h
+++ async/lib/io.h
@@ -12,9 +12,55 @@ int32_t io_write(int fd, const char *buf, uint32_t amount, size_t offset);
 int32_t io_read(int fd, char *buf, uint32_t amount, size_t offset);
 int32_t io_close(int fd);
 int32_t io_socket(int domain, int type, int protocol, int flags);
-int32_t io_accept(int fd, sockaddr *addr, socklen_t *addrlen, int flags);
-int32_t io_bind(int fd, sockaddr *addr, socklen_t addrlen);
+int32_t io_accept(int fd, struct sockaddr *addr, socklen_t *addrlen, int flags);
+int32_t io_bind(int fd, struct sockaddr *addr, socklen_t addrlen);
 int32_t io_listen(int fd, int queue);
+int32_t io_openat(int dfd, const char *path, int flags, mode_t mode);
+int32_t io_cancel(task_t *task, int flags);
+int32_t io_cancel_fd(int fd, int flags);
+int32_t io_connect(int fd, const struct sockaddr *addr, socklen_t addrlen);
+int32_t io_epoll_ctl(int epfd, int fd, int op, struct epoll_event *ev);
+int32_t io_fadvise(int fd, size_t offset, off_t len, int advice);
+int32_t io_fallocate( int fd, int mode, off_t offset, off_t len);
+int32_t io_fgetxattr(int fd, const char *name, const char *value, size_t len);
+int32_t io_fsetxattr(int fd, const char *name, const char *value, int flags, size_t len);
+int32_t io_files_update(int *fds, unsigned int nr_fds, int offset);
+int32_t io_fsync(int fd, unsigned int fsync_flags);
+int32_t io_link(const char *oldpath, const char *newpath, int flags);
+int32_t io_linkat(int olddfd, const char *oldpath, int newdfd, const char *newpath, int flags);
+int32_t io_madvise(void *addr, off_t length, int advice);
+int32_t io_mkdirat(int dfd, const char *path, mode_t mode);
+int32_t io_msg_ring(int fd, unsigned int len, __u64 data, unsigned int flags);
+int32_t io_multishot_accept(int fd, struct sockaddr *addr, socklen_t *addrlen, int flags);
+int32_t io_nop();
+int32_t io_setxattr(const char *name, const char *value, const char *path, int flags, size_t len);
+int32_t io_getxattr(const char *name, const char *value, const char *path, int flags, size_t len);
+int32_t io_poll_add(int fd, unsigned int poll_mask);
+int32_t io_poll_remove(task_t *task);
+int32_t io_poll_multishot(int fd, unsigned int poll_mask);
+int32_t io_poll_update(task_t *task, unsigned int poll_mask, unsigned int flags);
+int32_t io_provide_buffers(void *addr, int len, int nr, int bgid, int bid);
+int32_t io_remove_buffers(int nr, int bgid);
+int32_t io_recv(int sockfd, void *buf, size_t len, int flags);
+int32_t io_recvmsg(int fd, struct msghdr *msg, unsigned int flags);
+int32_t io_rename(const char *oldpath, const char *newpath);
+int32_t io_renameat(int olddfd, const char *oldpath, int newdfd, const char *newpath, int flags);
+int32_t io_send(int sockfd, const void *buf, size_t len, int flags);
+int32_t io_sendmsg(int fd, const struct msghdr *msg, unsigned int flags);
+int32_t io_shutdown(int fd, int how);
+int32_t io_splice(int fd_in, int64_t off_in, int fd_out, int64_t off_out, unsigned int nbytes, unsigned int splice_flags);
+int32_t io_statx(int dfd, const char *path, int flags, unsigned int mask, struct statx *statxbuf);
+int32_t io_symlink(const char *target, const char *linkpath);
+int32_t io_symlinkat(const char *target, int newdirfd, const char *linkpath);
+int32_t io_sync_file_range(int fd, unsigned int len, size_t offset, int flags);
+int32_t io_timeout(struct __kernel_timespec *ts, unsigned int count, unsigned int flags);
+int32_t io_timeout_remove(task_t *task, unsigned int flags);
+int32_t io_timeout_update(task_t *task,  struct __kernel_timespec *ts, unsigned int flags);
+int32_t io_unlink(const char *path, int flags);
+int32_t io_unlinkat(int dfd, const char *path, int flags);
+int32_t io_rw(int op, int fd, const void *addr, unsigned int len, size_t offset);
+
+
 #ifdef __cplusplus
 }
 
@@ -48,6 +94,182 @@ namespace io {
 
     static inline int32_t bind(int fd, sockaddr *addr, socklen_t addrlen) {
         return io_bind(fd, addr, addrlen);
+    }
+
+    static inline int32_t openat(int dfd, const char *path, int flags, mode_t mode) {
+        return io_openat(dfd, path, flags, mode);
+    }
+
+    static inline int32_t cancel(task_t *task, int flags) {
+        return io_cancel(task, flags);
+    }
+
+    static inline int32_t cancel_fd(int fd, int flags) {
+        return io_cancel_fd(fd, flags);
+    }
+
+    static inline int32_t connect(int fd, const struct sockaddr *addr, socklen_t addrlen) {
+        return io_connect(fd, addr, addrlen);
+    }
+
+    static inline int32_t epoll_ctl(int epfd, int fd, int op, struct epoll_event *ev) {
+        return io_epoll_ctl(epfd, fd, op, ev);
+    }
+
+    static inline int32_t fadvise(int fd, size_t offset, off_t len, int advice) {
+        return io_fadvise(fd, offset, len, advice);
+    }
+
+    static inline int32_t fallocate(int fd, int mode, off_t offset, off_t len) {
+        return io_fallocate(fd, mode, offset, len);
+    }
+
+    static inline int32_t fgetxattr(int fd, const char *name, const char *value, size_t len) {
+        return io_fgetxattr(fd, name, value, len);
+    }
+
+    static inline int32_t fsetxattr(int fd, const char *name, const char *value, int flags, size_t len) {
+        return io_fsetxattr(fd, name, value, flags, len);
+    }
+
+    static inline int32_t files_update(int *fds, unsigned int nr_fds, int offset) {
+        return io_files_update(fds, nr_fds, offset);
+    }
+
+    static inline int32_t fsync(int fd, unsigned int fsync_flags) {
+        return io_fsync(fd, fsync_flags);
+    }
+
+    static inline int32_t link(const char *oldpath, const char *newpath, int flags) {
+        return io_link(oldpath, newpath, flags);
+    }
+
+    static inline int32_t linkat(int olddfd, const char *oldpath, int newdfd, const char *newpath, int flags) {
+        return io_linkat(olddfd, oldpath, newdfd, newpath, flags);
+    }
+
+    static inline int32_t madvise(void *addr, off_t length, int advice) {
+        return io_madvise(addr, length, advice);
+    }
+
+    static inline int32_t mkdirat(int dfd, const char *path, mode_t mode) {
+        return io_mkdirat(dfd, path, mode);
+    }
+
+    static inline int32_t msg_ring(int fd, unsigned int len, __u64 data, unsigned int flags) {
+        return io_msg_ring(fd, len, data, flags);
+    }
+
+    static inline int32_t multishot_accept(int fd, struct sockaddr *addr, socklen_t *addrlen, int flags) {
+        return io_multishot_accept(fd, addr, addrlen, flags);
+    }
+
+    static inline int32_t nop() {
+        return io_nop();
+    }
+
+    static inline int32_t setxattr(const char *name, const char *value, const char *path, int flags, size_t len) {
+        return io_setxattr(name, value, path, flags, len);
+    }
+
+    static inline int32_t getxattr(const char *name, const char *value, const char *path, int flags, size_t len) {
+        return io_getxattr(name, value, path, flags, len);
+    }
+
+    static inline int32_t poll_add(int fd, unsigned int poll_mask) {
+        return io_poll_add(fd, poll_mask);
+    }
+
+    static inline int32_t poll_remove(task_t *task) {
+        return io_poll_remove(task);
+    }
+
+    static inline int32_t poll_multishot(int fd, unsigned int poll_mask) {
+        return io_poll_multishot(fd, poll_mask);
+    }
+
+    static inline int32_t poll_update(task_t *task, unsigned int poll_mask, unsigned int flags) {
+        return io_poll_update(task, poll_mask, flags);
+    }
+
+    static inline int32_t provide_buffers(void *addr, int len, int nr, int bgid, int bid) {
+        return io_provide_buffers(addr, len, nr, bgid, bid);
+    }
+
+    static inline int32_t remove_buffers(int nr, int bgid) {
+        return io_remove_buffers(nr, bgid);
+    }
+
+    static inline int32_t recv(int sockfd, void *buf, size_t len, int flags) {
+        return io_recv(sockfd, buf, len, flags);
+    }
+
+    static inline int32_t recvmsg(int fd, struct msghdr *msg, unsigned int flags) {
+        return io_recvmsg(fd, msg, flags);
+    }
+
+    static inline int32_t rename(const char *oldpath, const char *newpath) {
+        return io_rename(oldpath, newpath);
+    }
+
+    static inline int32_t renameat(int olddfd, const char *oldpath, int newdfd, const char *newpath, int flags) {
+        return io_renameat(olddfd, oldpath, newdfd, newpath, flags);
+    }
+
+    static inline int32_t send(int sockfd, const void *buf, size_t len, int flags) {
+        return io_send(sockfd, buf, len, flags);
+    }
+
+    static inline int32_t sendmsg(int fd, const struct msghdr *msg, unsigned int flags) {
+        return io_sendmsg(fd, msg, flags);
+    }
+
+    static inline int32_t shutdown(int fd, int how) {
+        return io_shutdown(fd, how);
+    }
+
+    static inline int32_t splice(int fd_in, int64_t off_in, int fd_out, int64_t off_out, unsigned int nbytes, unsigned int splice_flags) {
+        return io_splice(fd_in, off_in, fd_out, off_out, nbytes, splice_flags);
+    }
+
+    static inline int32_t statx(int dfd, const char *path, int flags, unsigned int mask, struct statx *statxbuf) {
+        return io_statx(dfd, path, flags, mask, statxbuf);
+    }
+
+    static inline int32_t symlink(const char *target, const char *linkpath) {
+        return io_symlink(target, linkpath);
+    }
+
+    static inline int32_t symlinkat(const char *target, int newdirfd, const char *linkpath) {
+        return io_symlinkat(target, newdirfd, linkpath);
+    }
+
+    static inline int32_t sync_file_range(int fd, unsigned int len, size_t offset, int flags) {
+        return io_sync_file_range(fd, len, offset, flags);
+    }
+
+    static inline int32_t timeout(struct __kernel_timespec *ts, unsigned int count, unsigned int flags) {
+        return io_timeout(ts, count, flags);
+    }
+
+    static inline int32_t timeout_remove(task_t *task, unsigned int flags) {
+        return io_timeout_remove(task, flags);
+    }
+
+    static inline int32_t timeout_update(task_t *task,  struct __kernel_timespec *ts, unsigned int flags) {
+        return io_timeout_update(task, ts, flags);
+    }
+
+    static inline int32_t unlink(const char *path, int flags) {
+        return io_unlink(path, flags);
+    }
+
+    static inline int32_t unlinkat(int dfd, const char *path, int flags) {
+        return io_unlinkat(dfd, path, flags);
+    }
+
+    static inline int32_t rw(int op, int fd, const void *addr, unsigned int len, size_t offset) {
+        return io_rw(op, fd, addr, len, offset);
     }
 
     static inline int32_t listen(int fd, int queue) {
--- async/src/io.cpp
+++ async/src/io.cpp
@@ -28,7 +28,7 @@ int32_t io_close(int fd) {
     });
 }
 
-}
+
 
 int32_t io_socket(int domain, int type, int protocol, int flags) {
     return async::request([domain, type, protocol, flags](io_uring_sqe *sqe) {
@@ -41,6 +41,273 @@ int32_t io_accept(int fd, sockaddr *addr, socklen_t *addrlen, int flags) {
         io_uring_prep_accept(sqe, fd, addr, addrlen, flags);
     });
 }
+
+int32_t io_openat(int dfd, const char *path, int flags, mode_t mode) {
+    return async::request([dfd, path, flags, mode](io_uring_sqe *sqe) {
+        io_uring_prep_openat(sqe, dfd, path, flags, mode);
+    });
+}
+
+int32_t io_cancel(task_t *task, int flags) {
+    return async::request([flags, task](io_uring_sqe *sqe) {
+        io_uring_prep_cancel(sqe, task, flags);
+    });
+}
+
+int32_t io_cancel_fd(int fd, int flags) {
+    return async::request([fd, flags](io_uring_sqe *sqe) {
+        io_uring_prep_cancel_fd(sqe, fd, flags);
+    });
+}
+
+int32_t io_connect(int fd, const sockaddr *addr, socklen_t addrlen) {
+    return async::request([fd, addr, addrlen](io_uring_sqe *sqe) {
+        io_uring_prep_connect(sqe, fd, addr, addrlen);
+    });
+}
+
+int32_t io_epoll_ctl(int epfd, int fd, int op, struct epoll_event *ev) {
+    return async::request([epfd, fd, op, ev](io_uring_sqe *sqe) {
+        io_uring_prep_epoll_ctl(sqe, epfd, fd, op, ev);
+    });
+}
+
+int32_t io_fadvise(int fd, size_t offset, off_t len, int advice) {
+    return async::request([fd, offset, len, advice](io_uring_sqe *sqe) {
+        io_uring_prep_fadvise(sqe, fd, offset, len, advice);
+    });
+}
+
+int32_t io_fallocate( int fd, int mode, off_t offset, off_t len) {
+    return async::request([fd, mode, offset, len](io_uring_sqe *sqe) {
+        io_uring_prep_fallocate(sqe, fd, mode, offset, len);
+    });
+}
+
+int32_t io_fgetxattr(int fd, const char *name, const char *value, size_t len) {
+    return async::request([fd, name, value, len](io_uring_sqe *sqe) {
+        io_uring_prep_fgetxattr(sqe, fd, name, value, len);
+    });
+}
+int32_t io_fsetxattr(int fd, const char *name, const char *value, int flags, size_t len) {
+    return async::request([fd, name, value, flags, len](io_uring_sqe *sqe) {
+        io_uring_prep_fsetxattr(sqe, fd, name, value, flags, len);
+    });
+}
+
+int32_t io_files_update(int *fds, unsigned int nr_fds, int offset) {
+    return async::request([fds, nr_fds, offset](io_uring_sqe *sqe) {
+        io_uring_prep_files_update(sqe, fds, nr_fds, offset);
+    });
+}
+
+int32_t io_fsync(int fd, unsigned int fsync_flags) {
+    return async::request([fd, fsync_flags](io_uring_sqe *sqe) {
+        io_uring_prep_fsync(sqe, fd, fsync_flags);
+    });
+}
+
+int32_t io_link(const char *oldpath, const char *newpath, int flags) {
+    return async::request([oldpath, newpath, flags](io_uring_sqe *sqe) {
+        io_uring_prep_link(sqe, oldpath, newpath, flags);
+    });
+}
+
+int32_t io_linkat(int olddfd, const char *oldpath, int newdfd, const char *newpath, int flags) {
+    return async::request([olddfd, oldpath, newdfd, newpath, flags](io_uring_sqe *sqe) {
+        io_uring_prep_linkat(sqe, olddfd, oldpath, newdfd, newpath, flags);
+    });
+}
+
+int32_t io_madvise(void *addr, off_t length, int advice) {
+    return async::request([addr, length, advice](io_uring_sqe *sqe) {
+        io_uring_prep_madvise(sqe, addr, length, advice);
+    });
+}
+
+
+int32_t io_mkdirat(int dfd, const char *path, mode_t mode) {
+    return async::request([dfd, path, mode](io_uring_sqe *sqe) {
+        io_uring_prep_mkdirat(sqe, dfd, path, mode);
+    });
+}
+
+int32_t io_msg_ring(int fd, unsigned int len, __u64 data, unsigned int flags) {
+    return async::request([fd, len, data, flags](io_uring_sqe *sqe) {
+        io_uring_prep_msg_ring(sqe, fd, len, data, flags);
+    });
+}
+
+int32_t io_multishot_accept(int fd, struct sockaddr *addr, socklen_t *addrlen, int flags) {
+    return async::request([fd, addr, addrlen, flags](io_uring_sqe *sqe) {
+        io_uring_prep_multishot_accept(sqe, fd, addr, addrlen, flags);
+    });
+}
+
+int32_t io_nop() {
+    return async::request([](io_uring_sqe *sqe) {
+        io_uring_prep_nop(sqe);
+    });
+}
+
+int32_t io_setxattr(const char *name, const char *value, const char *path, int flags, size_t len) {
+    return async::request([name, value, path, flags, len](io_uring_sqe *sqe) {
+        io_uring_prep_setxattr(sqe, name, value, path, flags, len);
+    });
+}
+
+int32_t io_getxattr(const char *name, const char *value, const char *path, int flags, size_t len) {
+    return async::request([name, value, path, flags, len](io_uring_sqe *sqe) {
+        io_uring_prep_setxattr(sqe, name, value, path, flags, len);
+    });
+}
+
+
+int32_t io_poll_add(int fd, unsigned int poll_mask) {
+    return async::request([fd, poll_mask](io_uring_sqe *sqe) {
+        io_uring_prep_poll_add(sqe, fd, poll_mask);
+    });
+}
+
+int32_t io_poll_remove(task_t *task) {
+    return async::request([task](io_uring_sqe *sqe) {
+        io_uring_prep_poll_remove(sqe, reinterpret_cast<__u64>(task));
+    });
+}
+
+int32_t io_poll_multishot(int fd, unsigned int poll_mask) {
+    return async::request([fd, poll_mask](io_uring_sqe *sqe) {
+        io_uring_prep_poll_multishot(sqe, fd, poll_mask);
+    });
+}
+
+int32_t io_poll_update(task_t *task, unsigned int poll_mask, unsigned int flags) {
+    return async::request([task, poll_mask, flags](io_uring_sqe *sqe) {
+        io_uring_prep_poll_update(sqe, reinterpret_cast<__u64>(task), reinterpret_cast<__u64>(task), poll_mask, flags);
+    });
+}
+
+int32_t io_provide_buffers(void *addr, int len, int nr, int bgid, int bid) {
+    return async::request([addr, len, nr, bgid, bid](io_uring_sqe *sqe) {
+        io_uring_prep_provide_buffers(sqe, addr, len, nr, bgid, bid);
+    });
+}
+
+int32_t io_remove_buffers(int nr, int bgid) {
+    return async::request([nr, bgid](io_uring_sqe *sqe) {
+        io_uring_prep_remove_buffers(sqe, nr, bgid);
+    });
+}
+
+int32_t io_recv(int sockfd, void *buf, size_t len, int flags) {
+    return async::request([sockfd, buf, len, flags](io_uring_sqe *sqe) {
+        io_uring_prep_recv(sqe, sockfd, buf, len, flags);
+    });
+}
+
+int32_t io_recvmsg(int fd, struct msghdr *msg, unsigned int flags) {
+    return async::request([fd, msg, flags](io_uring_sqe *sqe) {
+        io_uring_prep_recvmsg(sqe, fd, msg, flags);
+    });
+}
+
+int32_t io_rename(const char *oldpath, const char *newpath) {
+    return async::request([oldpath, newpath](io_uring_sqe *sqe) {
+        io_uring_prep_rename(sqe, oldpath, newpath);
+    });
+}
+
+int32_t io_renameat(int olddfd, const char *oldpath, int newdfd, const char *newpath, int flags) {
+    return async::request([olddfd, oldpath, newdfd, newpath, flags](io_uring_sqe *sqe) {
+        io_uring_prep_renameat(sqe, olddfd, oldpath, newdfd, newpath, flags);
+    });
+}
+
+int32_t io_send(int sockfd, const void *buf, size_t len, int flags) {
+    return async::request([sockfd, buf, len, flags](io_uring_sqe *sqe) {
+        io_uring_prep_send(sqe, sockfd, buf, len, flags);
+    });
+}
+
+int32_t io_sendmsg(int fd, const struct msghdr *msg, unsigned int flags) {
+    return async::request([fd, msg, flags](io_uring_sqe *sqe) {
+        io_uring_prep_sendmsg(sqe, fd, msg, flags);
+    });
+}
+
+int32_t io_shutdown(int fd, int how) {
+    return async::request([fd, how](io_uring_sqe *sqe) {
+        io_uring_prep_shutdown(sqe, fd, how);
+    });
+}
+
+int32_t io_splice(int fd_in, int64_t off_in, int fd_out, int64_t off_out, unsigned int nbytes, unsigned int splice_flags) {
+    return async::request([fd_in, off_in, fd_out, off_out, nbytes, splice_flags](io_uring_sqe *sqe) {
+        io_uring_prep_splice(sqe, fd_in, off_in, fd_out, off_out, nbytes, splice_flags);
+    });
+}
+
+int32_t io_statx(int dfd, const char *path, int flags, unsigned int mask, struct statx *statxbuf) {
+    return async::request([dfd, path, flags, mask, statxbuf](io_uring_sqe *sqe) {
+        io_uring_prep_statx(sqe, dfd, path, flags, mask, statxbuf);
+    });
+}
+
+int32_t io_symlink(const char *target, const char *linkpath) {
+    return async::request([target, linkpath](io_uring_sqe *sqe) {
+        io_uring_prep_symlink(sqe, target, linkpath);
+    });
+}
+
+int32_t io_symlinkat(const char *target, int newdirfd, const char *linkpath) {
+    return async::request([target, newdirfd, linkpath](io_uring_sqe *sqe) {
+        io_uring_prep_symlinkat(sqe, target, newdirfd, linkpath);
+    });
+}
+
+int32_t io_sync_file_range(int fd, unsigned int len, size_t offset, int flags) {
+    return async::request([fd, len, offset, flags](io_uring_sqe *sqe) {
+        io_uring_prep_sync_file_range(sqe, fd, len, offset, flags);
+    });
+}
+
+int32_t io_timeout(struct __kernel_timespec *ts, unsigned int count, unsigned int flags) {
+    return async::request([ts, count, flags](io_uring_sqe *sqe) {
+        io_uring_prep_timeout(sqe, ts, count, flags);
+    });
+}
+
+int32_t io_timeout_remove(task_t *task, unsigned int flags) {
+    return async::request([task, flags](io_uring_sqe *sqe) {
+        io_uring_prep_timeout_remove(sqe, reinterpret_cast<__u64>(task), flags);
+    });
+}
+
+int32_t io_timeout_update(task_t *task,  __kernel_timespec *ts, unsigned int flags) {
+    return async::request([task, flags, ts](io_uring_sqe *sqe) {
+        io_uring_prep_timeout_update(sqe, ts, reinterpret_cast<__u64>(task), flags);
+    });
+}
+
+int32_t io_unlink(const char *path, int flags) {
+    return async::request([path, flags](io_uring_sqe *sqe) {
+        io_uring_prep_unlink(sqe, path, flags);
+    });
+}
+
+int32_t io_unlinkat(int dfd, const char *path, int flags) {
+    return async::request([dfd, path, flags](io_uring_sqe *sqe) {
+        io_uring_prep_unlinkat(sqe, dfd, path, flags);
+    });
+}
+
+
+int32_t io_rw(int op, int fd, const void *addr, unsigned int len, size_t offset) {
+    return async::request([op, fd, addr, len, offset](io_uring_sqe *sqe) {
+        io_uring_prep_rw(op, sqe, fd, addr, len, offset);
+    });
+}
+
 
 int32_t io_bind(int fd, sockaddr *addr, socklen_t addrlen) {
     if (bind(fd, addr, addrlen) < 0) {
@@ -56,6 +323,7 @@ int32_t io_listen(int fd, int queue) {
     return 0;
 }
 
+}
 io::file::file(int fd, bool close) {
     m_fd = fd;
     m_should_close = close;
